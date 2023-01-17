eed1d7dcbfb61b5ab3369f1c1a050d862202de34
Anto2122 <73640854+antoniopopescu2122@users.noreply.github.com>
2023-01-18 00:17:50 +0200

    Add files via upload

---
+++ coffee website/reviewSubmit.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["name"]) {
+
+
+mail("popescuantonio75@gmail.com", "the subject line",
+
+
+$_POST["that's the first step, brother"]. "From: antonio.popescu02@e-uvt.ro");
+
+
+}
+
+
+?>
